3209d1a8e9971d2b61d2ad87387e9990052717a6
vert <vert@vert.com>
2021-03-19 14:12:43 -0400

    Deprecated in favor of upstream

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -71,7 +71,7 @@
 // Author info of this build printed to the host during boot and M115
 #define STRING_CONFIG_H_AUTHOR "(Vert)" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
-#define SHORT_BUILD_VERSION "Bugfix Build 504"
+#define SHORT_BUILD_VERSION "Deprecated B504" // Deprecated in favor of upstream 
 
 //START_HERE
 //-------------------------
